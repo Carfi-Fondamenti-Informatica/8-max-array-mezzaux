166c2fcf9760e755883fac80e8587c2f6664ffdf
Alessandro Mezzacasa <91669008+mezzaux@users.noreply.github.com>
2021-12-08 17:36:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#include "lib.h"
 int main(){
-  return 0;
+    using namespace std;
+    int n=0;
+    cin>> n;
+    int numeri[n];
+    for(int i;i<n;i++){
+        cin>>numeri[i];
+    }
+    cout<<funzione(numeri,n);
+
+    return 0;
 }
