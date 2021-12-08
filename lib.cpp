6342e1aeac98f0a45763b324a6982f5f5000e5d3
Alessandro Mezzacasa <91669008+mezzaux@users.noreply.github.com>
2021-12-08 17:35:39 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
-
+#include "lib.h"
+int funzione(const int numeri[],int n){
+    int max=numeri[0];
+    for(int i=1;i<n;i++){
+        if(numeri[i]>max){
+            max=numeri[i];
+        }
+    }
+    return max;
+}
