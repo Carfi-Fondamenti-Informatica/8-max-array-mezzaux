5a212c25b3acd8edfa683945fe437a7e0f7427c7
Alessandro Mezzacasa <91669008+mezzaux@users.noreply.github.com>
2021-12-08 17:50:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include "lib.h"
-
 int main(){
     using namespace std;
     int n=0;
     cin>> n;
-    if(n>1){
     float numeri[n];
     for(int i;i<n;i++){
         cin>>numeri[i];
     }
-    cout<<funzione(numeri,n);}
+    cout<<funzione(numeri,n);
 
     return 0;
 }
